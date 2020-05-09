5004816fff13802e4dddd745e7b7b8b41e2f88a6
divyshri <divyanshu4shrivastava@gmail.com>
2020-05-09 23:23:32 +0530

    Keyboard movement template

--- pygame_keyboard.py
+++ pygame_keyboard.py
@@ -1,23 +1,54 @@
 import pygame, sys, os
 from pygame.locals import *
 
-red = [255, 0 , 0]
+catx = 10
+caty = 10
+screen = 0
 
-pygame.init()
+def myQuit():
+	pygame.quit()
+	sys.exit(0)
 
-window = pygame.display.set_mode((1000,600))
-pygame.display.set_caption('Slither.eat')
+def check_inputs(events):
+	global catx,caty,screen
 
-
-screen  = pygame.display.get_surface()
-screen.fill(red)
-pygame.display.set_caption("Snake")
-pygame.display.flip()
-
-while True:
-	for event in pygame.event.get():
-		print(event)
+	for event in events:
 		if event.type == QUIT:
-			pygame.quit()
-			sys.exit()
+			quit()
+		else:
+			if event.type == KEYDOWN:
+				if event.key == K_ESCAPE:
+					myQuit()
+				elif event.key == K_LEFT or event.key == K_a:
+					catx -= 5
+					print("maove Rectangle Left")
+				elif event.key == K_RIGHT or event.key == K_d:
+					catx += 5
+					print("Move Rectangle Right")
+				elif event.key == K_UP or event.key == K_w:
+					caty -= 5
+					print("Move Rectangle UP")
+				elif event.key == K_DOWN or event.key == K_s:
+					caty += 5
+					print("Move Rectangle Down")
+				else:
+					print(event.key)
+	screen.fill((0,0,0))
+	pygame.draw.rect(screen,(255,255,255),(catx,caty,50,100))
 	pygame.display.update()
+
+
+
+def main():
+	global screen
+	pygame.init()
+	window = pygame.display.set_mode((600,600))
+	pygame.display.set_caption('KeyBoard Move')
+	screen  = pygame.display.get_surface()
+	pygame.display.update()
+	
+	while True:
+		check_inputs(pygame.event.get())
+
+
+main()
